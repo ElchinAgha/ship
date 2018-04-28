8ed097b9e31a2092fb313445eacc13c6f8e15624
Elçin Ağayev <mr.aghayev675@gmail.com>
2018-04-29 00:12:10 +0400

    Add files via upload

---
+++ Ship/Ship/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Ship
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PcbShip = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PcbShip)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PcbShip
+            // 
+            this.PcbShip.BackColor = System.Drawing.Color.Transparent;
+            this.PcbShip.Image = ((System.Drawing.Image)(resources.GetObject("PcbShip.Image")));
+            this.PcbShip.Location = new System.Drawing.Point(451, 180);
+            this.PcbShip.Name = "PcbShip";
+            this.PcbShip.Size = new System.Drawing.Size(201, 255);
+            this.PcbShip.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PcbShip.TabIndex = 0;
+            this.PcbShip.TabStop = false;
+            this.PcbShip.Click += new System.EventHandler(this.PcbShip_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(655, 447);
+            this.Controls.Add(this.PcbShip);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.PcbShip)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PcbShip;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Ship/Ship/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ship
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            PcbShip.Left = this.Width - 280;
+        }
+
+        private void PcbShip_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        private void MyTimer_Tick(object sender,EventArgs eArgs)
+        {
+
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          for(int i = this.Width - 280; i > 10; i--)
+            {
+                PcbShip.Left = i;
+            }
+            PcbShip.Image.RotateFlip(RotateFlipType.Rotate270FlipXY);
+
+        }
+    }
+}
